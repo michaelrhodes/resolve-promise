b67e28e32ab8f27931e3a723b24dceae0d72ec7d
michaelrhodes <spam@michaelrhod.es>
2014-11-18 20:46:37 +1000

    Added tests for all promise implemetations.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function (promise, callback) {
   promise.then(success, error)
 
   function error (value) {
-    callback(value || new Error('Promise rejected'))
+    callback(value || new Error)
   }
 
   function success (value) {
--- test.js
+++ test.js
@@ -1,33 +1,70 @@
 var test = require('tape')
 var broken = require('./')
 
-var Promise = require('promise')
+var promise = require('promise')
 var bluebird = require('bluebird')
 var deferred = require('deferred')
-var when = require('when')
 var q = require('q')
-
-var run = function(assert, resolved, rejected) {
-  assert.plan(2)
-
-  broken(rejected, function(error) {
-    assert.ok(error.message === 'done', 'rejected promise produces an error')
-  })
-
-  broken(resolved, function(error, value) {
-    assert.ok(!error && value === 'done', 'resolved promise returns no error')
-  })
-}
+var when = require('when')
 
 test('npm.im/promise', function(assert) {
+  standard(assert, promise) 
+})
 
+test('npm.im/bluebird', function(assert) {
+  standard(assert, bluebird) 
+})
+
+test('npm.im/deferred', function(assert) {
+  nonstandard(assert, deferred)
+})
+
+test('npm.im/q', function(assert) {
+  nonstandard(assert, q.defer)
+})
+
+test('npm.im/when', function(assert) {
+  var resolved = when.promise(function(resolve, reject) {
+    resolve('done')
+  })
+
+  var rejected = when.promise(function(resolve, reject) {
+    reject()
+  })
+
+  go(assert, resolved, rejected)
+})
+
+function nonstandard (assert, defer) {
+  var resolved = defer()
+  var rejected = defer()
+
+  resolved.resolve('done')
+  rejected.reject()
+
+  go(assert, resolved.promise, rejected.promise)
+}
+
+function standard (assert, Promise) {
   var resolved = new Promise(function(resolve, reject) {
     resolve('done')
   })
 
   var rejected = new Promise(function(resolve, reject) {
-    reject(new Error('done'))
+    reject()
   })
 
-  run(assert, resolved, rejected)
-})
+  go(assert, resolved, rejected)
+}
+
+function go(assert, resolved, rejected) {
+  assert.plan(2)
+
+  broken(rejected, function(error, value) {
+    assert.ok(error && !value, 'rejected')
+  })
+
+  broken(resolved, function(error, value) {
+    assert.ok(!error && value === 'done', 'resolved')
+  })
+}
